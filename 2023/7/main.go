71d0eec13d1034674204787eab2923d09fdd8365
Thom Oman <toman@gocardless.com>
2023-12-10 17:53:43 +0000

    day 7, part 2

--- 2023/7/main.go
+++ 2023/7/main.go
@@ -27,57 +27,64 @@ func main() {
 		h := newHand(f[0], b)
 		hds = append(hds, h)
 	}
-	
-	sort.Slice(hds, func(i,j int) bool {
-		x,y := hds[i], hds[j]
-		if x.ct == y.ct {
-			return compareCards(x.cards, y.cards)
+
+	sort.SliceStable(hds, func(i, j int) bool {
+		// This returns true if i should be ranked lower j
+		x, y := hds[i], hds[j]
+
+		if x.ct != y.ct {
+			return x.ct < y.ct
 		}
-		return x.ct < y.ct
+		return compareCards(x, y)
 	})
 
 	var t int
-	for i := range hds {
-		t += (i+1) * hds[i].bid	
+	for i, h := range hds {
+		t += (i + 1) * h.bid
 	}
 	fmt.Println("Total", t)
 }
 
-func compareCards(a,b []byte) bool {
-	for idx := range a {
-		i,j := a[idx], b[idx]
+func compareCards(a, b hand) bool {
+	for idx := range a.cards {
+		i, j := a.cards[idx], b.cards[idx]
 
-		if i == j {
-			continue
+		if i != j {
+			return compareValue(i, j)
 		}
-
-		if isNumber(i) || isNumber(j) {
-			// If both are numbers, simply compare the byte values as proxy
-			if isNumber(i) && isNumber(j) {
-				return i < j
-			}
-
-			// if only first is a number then other isn't, otherise the other is a number
-			// whilst the first is not. The number always lose
-			return isNumber(i)
-		}
-
-		return bytes.IndexByte(letterCardsSorted, i) < bytes.IndexByte(letterCardsSorted, j) 
 	}
 
-	// Identical, so choose one loser
-	return false
+	panic("cant split")
+}
+
+func compareValue(i, j byte) bool {
+	// If either card is J then we don't have to check further
+	if i == cardJ || j == cardJ {
+		return i == cardJ
+	}
+
+	// If both are numbers, compare the byte values as proxy
+	if isNumber(i) && isNumber(j) {
+		return i < j
+	}
+
+	// if only first is a number then other isn't or vice versa, the the number loses
+	if isNumber(i) || isNumber(j) {
+		return isNumber(i)
+	}
+
+	ii := bytes.IndexByte(letterCardsSorted, i)
+	ij := bytes.IndexByte(letterCardsSorted, j)
+	if ii < 0 || ij < 0 {
+		panic(fmt.Sprintf("%v\t%v", ii, ij))
+	}
+	return ii < ij
 }
 
 func isNumber(b byte) bool {
 	return b >= 48 && b <= 57
 }
 
-// A = 65
-// K = 75
-// Q = 81
-// J = 74
-// T = 84
 var (
 	cardA byte = 65
 	cardK byte = 75
@@ -85,12 +92,12 @@ var (
 	cardJ byte = 74
 	cardT byte = 84
 )
-var letterCardsSorted = []byte{cardA, cardK, cardQ, cardJ, cardT}
+var letterCardsSorted = []byte{cardT, cardQ, cardK, cardA}
 
 type hand struct {
 	cards []byte
-	bid int
-	ct cardType
+	bid   int
+	ct    cardType
 }
 
 type cardType int
@@ -107,58 +114,113 @@ const (
 
 func newHand(cards string, b int) hand {
 	bs := []byte(cards)
-	// var jids []int
 
-	var h hand
-	h.cards = bs
-	h.bid = b
-
-	cs := make(map[byte]int)
-	for i := range bs {
-		// if bs[i] == 74 {
-		// 	jids = append(jids, i)
-		// }
-		cs[bs[i]]++
+	return hand{
+		cards: bs,
+		bid:   b,
+		ct:    determineCardType(bs),
 	}
-	if len(cs) == len(bs) {
-		h.ct = highCard
-		return h
-	} 
-
-	if len(cs) == 1 {
-		h.ct = fiveOfAKind
-		return h
-	}
-	var ps, ts int
-	for j := range cs {
-		if cs[j] == 2 {
-			ps++
-		}
-		if cs[j] == 3 {
-			ts++
-		}
-		if cs[j] == 4 {
-			h.ct = fourOfAKind
-			return h
-		}
-	}
-
-	if ts > 0 {
-		if ps > 0 {
-			h.ct = fullHouse
-			return h
-		}
-		h.ct = threeOfAKind
-		return h
-	}
-
-	if ps == 2 {
-		h.ct = twoPair
-		return h
-	} else if ps == 1 {
-		h.ct = onePair
-		return h
-	}
-
-	return h
 }
+
+func determineCardType(bs []byte) cardType {
+	ccount := make(map[byte]int)
+
+	for i := range bs {
+		ccount[bs[i]]++
+	}
+
+	jc, _ := ccount[cardJ]
+	startType := cardTypeForCounts(ccount)
+	return improvedCardType(startType, jc)
+}
+
+func improvedCardType(startType cardType, jc int) cardType {
+	// No jacks/jokers or hand cannot be improved
+	if jc == 0 || startType == fiveOfAKind {
+		return startType
+	}
+
+	switch startType {
+	case fourOfAKind:
+		// Change joker to match other 4 or change all
+		// 4 jokers to match 5th card
+		if jc == 1 || jc == 4 {
+			return fiveOfAKind
+		}
+	case fullHouse:
+		// Similarly, change minoriy to match majority
+		if jc == 2 || jc == 3 {
+			return fiveOfAKind
+		}
+	case threeOfAKind:
+		// If the 3 are jokers, then no improvement can be made
+		if jc == 3 {
+			return fourOfAKind
+		}
+		// swap joker to match other 3
+		if jc == 1 {
+			return fourOfAKind
+		}
+	case twoPair:
+		// One of the pairs is jokers so make them match other pair
+		if jc == 2 {
+			return fourOfAKind
+		}
+
+		// Turn one of the pairs into a three
+		if jc == 1 {
+			return fullHouse
+		}
+	case onePair:
+		// If pair is jacks then no improvement can be made
+		if jc == 2 {
+			return threeOfAKind
+		}
+		// Otherwise, if one joker then we can turn pair into 3
+		if jc == 1 {
+			return threeOfAKind
+		}
+	case highCard:
+		// Can only create a pair with one of the others
+		if jc == 1 {
+			return onePair
+		}
+	}
+	return startType
+}
+
+func cardTypeForCounts(ccount map[byte]int) cardType {
+	var t cardType
+	var cs []int
+
+	for _, c := range ccount {
+		cs = append(cs, c)
+
+		switch c {
+		case 5:
+			if t == highCard {
+				t = fiveOfAKind
+			}
+		case 4:
+			if t == highCard {
+				t = fourOfAKind
+			}
+		case 3:
+			if t == onePair {
+				t = fullHouse
+			} else if t == highCard {
+				t = threeOfAKind
+			}
+		case 2:
+			if t == threeOfAKind {
+				t = fullHouse
+			} else if t == onePair {
+				t = twoPair
+			} else if t == highCard {
+				t = onePair
+			}
+		}
+	}
+	return t
+}
+
