750b65348ffcff4cc18bcaff179ad7fed8c19ce5
Thom Oman <toman@gocardless.com>
2023-12-11 19:10:25 +0000

    clean up day 5

--- 2023/5/main.go
+++ 2023/5/main.go
@@ -13,20 +13,91 @@ import (
 
 var (
 	mapmatch = regexp.MustCompile("[map:]")
+	m        *mapping
+	mappings []*mapping
+	sranges []string
 )
 
 func main() {
-	lines := parseInputFile()
-	// for _, s := range {
-	// 	n, _ := strconv.Atoi(s)
-	// 	seeds = append(seeds, n)
-	// }
-	// x_start, x_len, y_start, y_len := seeds[0], seeds[1], seeds[2], seeds[3]
+	parseInputFile()
+	walkRanges()
+}
 
-	var (
-		m        *mapping
-		mappings []*mapping
-	)
+func walkRanges() {
+	curMin := math.MaxInt
+
+	var i int
+	for i < len(sranges) {
+		start, _ := strconv.Atoi(sranges[i])
+		length, _ := strconv.Atoi(sranges[i+1])
+
+		for k := start; k < start + length; k++ {
+			w := k
+
+			for j := range mappings {
+				m = mappings[j]
+				w = m.Map(w)
+			}
+
+			if curMin > w {
+				curMin = w
+			}
+		}
+
+		i += 2
+	}
+
+	fmt.Println("Min location:", int(curMin))
+}
+
+type mapping struct {
+	from, to    string
+	ranges []nrange
+}
+
+type nrange struct {
+	dest, source, length int
+}
+
+func NewMapping(l string) *mapping {
+	maps := strings.Split(strings.Split(l, "map:")[0], "-")
+
+	return &mapping{from: maps[0], to: maps[2]}
+}
+
+func (m *mapping) Map(val int) int {
+	for i := range m.ranges {
+		r := m.ranges[i]
+
+		if r.source <= val && val < r.source + r.length {
+			return val + (r.dest - r.source)
+		}
+	}
+	return val
+}
+
+func (m *mapping) AddRange(dest, source, length int) {
+	m.ranges = append(m.ranges, nrange{dest, source, length})
+}
+
+func parseInputFile() {
+	f, err := os.Open("inputs.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(f)
+	var lines []string
+	for {
+		l, err := r.ReadBytes('\n')
+		if len(l) == 0 {
+			break
+		}
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		lines = append(lines, string(l[:len(l)-1]))
+	}
 
 	for _, l := range lines[1:] {
 		if len(l) == 0 {
@@ -47,80 +118,5 @@ func main() {
 		m.AddRange(rval[0], rval[1], rval[2])
 	}
 
-	sranges := strings.Fields(strings.Split(lines[0], ":")[1]) 
-
-	var i int
-	curMin := math.MaxInt
-	for i < len(sranges) {
-		start, _ := strconv.Atoi(sranges[i])
-		length, _ := strconv.Atoi(sranges[i+1])
-
-		for k := start; k < start + length; k++ {
-			w := k
-
-			for j := range mappings {
-				m = mappings[j]
-				w = m.Map(w)
-			}
-			curMin = math.Min(w, curMin)
-		}
-
-		i += 2
-	}
-
-	fmt.Println("Min location:", int(curMin))
-}
-
-type mapping struct {
-	from, to    string
-	ranges []nrange
-}
-
-type nrange struct {
-	dest, source, length int
-}
-
-func NewMapping(l string) *mapping {
-	maps := strings.Split(strings.Split(l, "map:")[0], "-")
-	return &mapping{
-		from:        maps[0],
-		to:          maps[2],
-	}
-}
-
-func (m *mapping) Map(val int) int {
-	for i := range m.ranges {
-		r := m.ranges[i]
-
-		if r.source <= val && val < r.source + r.length {
-			return val + (r.dest - r.source)
-		}
-	}
-	return val
-}
-
-func (m *mapping) AddRange(dest, source, length int) {
-	m.ranges = append(m.ranges, nrange{dest, source, length})
-}
-
-func parseInputFile() []string {
-	f, err := os.Open("inputs.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	r := bufio.NewReader(f)
-	var lines []string
-	for {
-		l, err := r.ReadBytes('\n')
-		if len(l) == 0 {
-			break
-		}
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		lines = append(lines, string(l[:len(l)-1]))
-	}
-
-	return lines
+	sranges = strings.Fields(strings.Split(lines[0], ":")[1]) 
 }
